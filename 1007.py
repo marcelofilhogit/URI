ab8ea78c3c834b4665689846ea3d3cb9324b7fe6
Marcelo Gonçalves dos Reis Filho <marcelofilhoufu@gmail.com>
2018-11-28 10:22:40 -0200

    Diferença

---
+++ 1007.py
@@ -0,0 +1,11 @@
+def diferenca():
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
+    num4 = int(input())
+
+    diferenca = (num1 * num2) - (num3 * num4)
+
+    print("DIFERENCA =",diferenca)
+
+diferenca()
