b66b344e65dc6611a34ff610a1359eb7ec9ecc8e
Marcelo Gonçalves dos Reis Filho <marcelofilhoufu@gmail.com>
2018-11-28 10:20:47 -0200

    Média 2

---
+++ 1006.py
@@ -0,0 +1,11 @@
+def media():
+    num1 = float(input())
+    num2 = float(input())
+    num3 = float(input())
+
+    media = (((num1 * 2) + (num2 * 3) + (num3 * 5)) / 10)
+
+    print("MEDIA = %0.1f"%media)
+
+media()
+
